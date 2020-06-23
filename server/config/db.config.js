6892a2023a445637c035b13573abebf1447cca28
surinder4736 <surinderkumardhanju@gmail.com>
2020-06-23 22:04:20 +0530

    commit first time server

---
+++ server/config/db.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    HOST: "162.222.225.88",
+    USER: "fss",
+    PASSWORD: "k_k65C7b",
+    DB: "atesttfn_altweberp"
+    // HOST: "LENOVO-PC\MSSQLSERVER14",
+    // USER: "sa",
+    // PASSWORD: "123456",
+    // DB: "erp"
+    
+  };

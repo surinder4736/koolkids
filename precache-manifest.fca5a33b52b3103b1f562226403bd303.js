36da383a9177f60669d7655027ca4739323cc7bc
surinder4736 <surinderkumardhanju@gmail.com>
2020-06-23 23:16:25 +0530

    Updates

---
+++ precache-manifest.fca5a33b52b3103b1f562226403bd303.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123f89a6992f4da6b616d0667a156acc",
+    "url": "/koolkids/index.html"
+  },
+  {
+    "revision": "231eeab639de62899b2d",
+    "url": "/koolkids/static/css/main.1d61bb7f.chunk.css"
+  },
+  {
+    "revision": "ec6719ee7527964c6c30",
+    "url": "/koolkids/static/js/2.d95f0a06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/koolkids/static/js/2.d95f0a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231eeab639de62899b2d",
+    "url": "/koolkids/static/js/main.1c582c0d.chunk.js"
+  },
+  {
+    "revision": "8967e3d6e54196a02219",
+    "url": "/koolkids/static/js/runtime-main.bf5a58db.js"
+  },
+  {
+    "revision": "8a94d1fdf824930c8451bacbdf20c806",
+    "url": "/koolkids/static/media/caartoon.8a94d1fd.gif"
+  },
+  {
+    "revision": "93ab757ec1e21fd756649e6a7fdd6f1e",
+    "url": "/koolkids/static/media/food.93ab757e.jpg"
+  },
+  {
+    "revision": "05916ed1643b8f9913953ceef1e041a3",
+    "url": "/koolkids/static/media/homekoolkids.05916ed1.jpg"
+  },
+  {
+    "revision": "2e5203c43fd240b35ce97431b83e2670",
+    "url": "/koolkids/static/media/homekoolkids2.2e5203c4.jpg"
+  },
+  {
+    "revision": "db574f2546e0c5f657af8599dc394372",
+    "url": "/koolkids/static/media/homekoolkids4.db574f25.jpg"
+  },
+  {
+    "revision": "fa2334a7bb228739dc979c49494e3881",
+    "url": "/koolkids/static/media/homwkoolkids3.fa2334a7.jpg"
+  },
+  {
+    "revision": "0768c020c0fa1573b84bfdf665d341b7",
+    "url": "/koolkids/static/media/instagram.0768c020.jpg"
+  },
+  {
+    "revision": "b3f2ef00cc99e252ae37823673bc4069",
+    "url": "/koolkids/static/media/mission.b3f2ef00.jpg"
+  },
+  {
+    "revision": "d5da4df4f772db091bae21bb45bf9478",
+    "url": "/koolkids/static/media/motivation.d5da4df4.jpg"
+  },
+  {
+    "revision": "f28baa3d541dc5f29c884595edc844c8",
+    "url": "/koolkids/static/media/origin.f28baa3d.jpg"
+  },
+  {
+    "revision": "31b4dad3146d2e3829855b0015ea7cb4",
+    "url": "/koolkids/static/media/perks.31b4dad3.jpg"
+  },
+  {
+    "revision": "32b822c1837a75b0d5ee20954787faae",
+    "url": "/koolkids/static/media/rupee.32b822c1.jpg"
+  },
+  {
+    "revision": "e24c83b894cdbd2e73ad2b9e58df1868",
+    "url": "/koolkids/static/media/twitter.e24c83b8.png"
+  }
+]);
